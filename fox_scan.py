427af4733c1795919aae11b53e2be396d40270c3
User <146877055+icastro-solucomglobal@users.noreply.github.com>
2024-03-12 12:36:51 -0600

    Update Fox-scan

--- fox_scan.py
+++ fox_scan.py
@@ -2,88 +2,141 @@ import subprocess
 import re
 import pyperclip
 import os
+import pwd
+from tqdm import tqdm
 
-def create_directory(location, directory_name):
-    try:
-        if location.lower() == 'here':
-            location = os.getcwd()  # Use the current working directory
-        elif not os.path.isabs(location):
-            location = os.path.join(os.getcwd(), location)  # Make it an absolute path
 
-        # Check if the directory name is valid
-        if not is_valid_directory_name(directory_name):
-            raise ValueError("Invalid directory name. Please follow the naming conventions.")
+class DirectoryCreator:
+    def __init__(self):
+        self.new_directory_path = None
 
-        # Create the new directory
-        new_directory_path = os.path.join(location, directory_name)
-        os.makedirs(new_directory_path, exist_ok=True)
+    def create_directory(self, location, directory_name):
+        try:
+            if location.lower() == 'here':
+                location = os.getcwd()  # Use the current working directory
+            elif not os.path.isabs(location):
+                location = os.path.join(os.getcwd(), location)  # Make it an absolute path
 
-        print(f"Directory '{directory_name}' created successfully at '{new_directory_path}'")
+            # Check if the directory name is valid
+            if not self.is_valid_directory_name(directory_name):
+                raise ValueError("Invalid directory name. Please follow the naming conventions.")
 
-        # Change the current working directory to the newly created directory
-        os.chdir(new_directory_path)
+            # Create the new directory
+            self.new_directory_path = os.path.join(location, directory_name)
+            os.makedirs(self.new_directory_path, exist_ok=True)
+
+            # Change the ownership of the directory to the current user
+            uid = pwd.getpwnam(os.getlogin()).pw_uid
+            gid = pwd.getpwnam(os.getlogin()).pw_gid
+            os.chown(self.new_directory_path, uid, gid)
+
+            print(f"Directory '{directory_name}' created successfully at '{self.new_directory_path}'")
+
+            # Change the current working directory to the newly created directory
+            os.chdir(self.new_directory_path)
+
+            return True
+        except PermissionError as pe:
+            print(f"Error: {pe}")
+            question = input(
+                "\nInsufficient privileges to change directory. Do you want to proceed with elevated privileges (sudo)? (y/n): ")
+            if question.lower() == 'y':
+                sudo_password = input("Enter your sudo password: ")
+                sudo_command = f"echo {sudo_password} | sudo -S chown $USER:$USER {self.new_directory_path}"
+                subprocess.run(sudo_command, shell=True)
+                os.chdir(self.new_directory_path)
+                print(
+                    f"Directory '{directory_name}' created successfully at '{self.new_directory_path}' with elevated privileges.")
+                return True
+            else:
+                return False
+        except Exception as e:
+            print(f"Error creating directory: {e}")
+            return False
+
+    @staticmethod
+    def is_valid_directory_name(directory_name):
+        # Avoid spaces and special characters
+        if not re.match("^[a-zA-Z0-9_-]*$", directory_name):
+            return False
+
+        reserved_keywords = {'dev', 'proc', 'sys', 'tmp', 'bin', 'sbin', 'etc'}
+        if directory_name.lower() in reserved_keywords:
+            return False
+
+        if directory_name.startswith('.'):
+            return False
+
+        if directory_name in {'..', '.'}:
+            return False
+
+        if directory_name.lower().startswith('/dev/'):
+            return False
 
         return True
-    except PermissionError as pe:
-        print(f"Error: {pe}")
-        question = input("\nInsufficient privileges to change directory. Do you want to proceed with elevated privileges (sudo)? (y/n): ")
-        if question.lower() == 'y':
-            sudo_password = input("Enter your sudo password: ")
-            sudo_command = f"echo {sudo_password} | sudo -S chown $USER:$USER {new_directory_path}"
-            subprocess.run(sudo_command, shell=True)
-            os.chdir(new_directory_path)
-            print(f"Directory '{directory_name}' created successfully at '{new_directory_path}' with elevated privileges.")
-            return True
-        else:
-            return False
-    except Exception as e:
-        print(f"Error creating directory: {e}")
-        return False
 
 
-def is_valid_directory_name(directory_name):
-    # Avoid spaces and special characters
-    if not re.match("^[a-zA-Z0-9_-]*$", directory_name):
-        return False
-
-    reserved_keywords = {'dev', 'proc', 'sys', 'tmp', 'bin', 'sbin', 'etc'}
-    if directory_name.lower() in reserved_keywords:
-        return False
-
-    if directory_name.startswith('.'):
-        return False
-    
-    if directory_name in {'..', '.'}:
-        return False
-
-    if directory_name.lower().startswith('/dev/'):
-        return False
-
-    return True
-
-def extract_open_ports(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            # Read the content of the file
-            content = file.read()
-
-            # Use regex to extract open ports
-            open_ports_match = re.search(r'(\d+)/open/tcp//[^/]*/', content)
-
-            if open_ports_match:
-                open_ports = re.findall(r'(\d+)/open/tcp//[^/]*/', content)
-                
-                if open_ports:
+class PortScanner:
+    @staticmethod
+    def extract_open_ports(file_path):
+        try:
+            with open(file_path, 'r') as file:
+                content = file.read()
+                open_ports_match = re.findall(r'(\d+)/open/', content)
+                if open_ports_match:
+                    open_ports = [port for port in open_ports_match]
                     return open_ports
                 else:
                     print("No open ports found.")
                     return []
-            else:
-                print("No open ports found.")
-                return []
-    except FileNotFoundError:
-        print(f"File '{file_path}' not found.")
-        return []
+        except FileNotFoundError:
+            print(f"File '{file_path}' not found.")
+            return []
+
+    @staticmethod
+    def run_second_scan(open_ports, target):
+        if open_ports:
+            try:
+                ports_str = ','.join(open_ports)
+                # Specify a user-writable file path for the output
+                output_file = os.path.join(os.getcwd(), 'full.txt')
+                print("\n##### RUNNING SERVICES SCAN #####\n")
+                with open(os.devnull, 'w') as devnull:
+                    subprocess.run(['sudo', 'nmap', '-p', ports_str, '-sCV', '-n', '-Pn', '-oN', 'full.txt', target], check=True,encoding='utf-8', stdout=devnull, stderr=subprocess.STDOUT)
+                print("\n##### SCAN ENDED #####)\n")
+                print("\nSecond scan completed. Results saved in info.txt\n")
+                relevant_info = PortScanner.extract_service_info(output_file)
+                PortScanner.save_to_file(relevant_info, "info.txt")
+                choice = input("Do you want to see a preview of the extracted information? (yes/no): ").lower()
+                if choice == "yes":
+                    print("\n".join(relevant_info))
+                print("Information saved to info.txt")
+            except subprocess.CalledProcessError as e:
+                print(f"\nError during the second scan: {e}")
+        else:
+            print("\nNo open ports to perform the second scan.")
+
+    @staticmethod
+    def extract_service_info(file_path):
+        try:
+            with open(file_path, 'r') as file:
+                lines = file.readlines()
+                relevant_info = []
+                for line in lines:
+                    if "PORT" in line and "STATE" in line and "SERVICE" in line and "VERSION" in line:
+                        relevant_info.append(line.strip())
+                    elif "/tcp" in line:
+                        relevant_info.append(line.strip())
+                return relevant_info
+        except FileNotFoundError:
+            print(f"\nFile '{file_path}' not found.")
+
+    @staticmethod
+    def save_to_file(info, filename):
+        with open(filename, "w") as file:
+            for line in info:
+                file.write(line + "\n")
+
 
 def copy_to_clipboard(data):
     try:
@@ -92,83 +145,51 @@ def copy_to_clipboard(data):
     except pyperclip.PyperclipException as e:
         print(f"Error copying to clipboard: {e}")
 
-def run_second_scan(open_ports, target):
-    if open_ports:
-        try:
-            # Create a string of open ports separated by commas
-            ports_str = ','.join(open_ports)
 
-            # Run the second scan
-            subprocess.run(['sudo', 'nmap', '-p', ports_str, '-sCV', '-n', '-Pn', '-oN', 'info.txt', target], check=True)
+def main():
+    # Prompt user for target IP address
+    target = input("\nProvide the target IP address: ")
 
-            print("Second scan completed. Results saved in info.txt")
+    question = input(
+        "\nThis program creates files. If a file exists with the same name, it will overwrite it. "
+        "To avoid future problems, the program will move or create a new directory.\n\n"
+        "Do you want to proceed with the process of creating a directory? (y/n): ")
 
-            # Extract and print service information from info.txt
-            extract_service_info('info.txt')
-        except subprocess.CalledProcessError as e:
-            print(f"\nError during the second scan: {e}")
-    else:
-        print("\nNo open ports to perform the second scan.")
-
-def extract_service_info(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            # Read the content of the file
-            content = file.read()
-
-            # Use regex to extract service information
-            service_info_match = re.search(r'PORT\s+STATE\s+SERVICE\s+VERSION\n(.+?)\n# Nmap done', content, re.DOTALL)
-
-            if service_info_match:
-                service_info_str = service_info_match.group(1)
-                # Use regex to extract relevant details for each service
-                service_details = re.findall(r'(\d+/tcp)\s+(\w+)\s+(\w+)\s+([^/]+)/', service_info_str)
-
-                if service_details:
-                    # Print the header
-                    print("\nPorts         State/Service        Version")
-                    # Print the extracted information for each service
-                    for details in service_details:
-                        print(f"{details[0]}    {details[1]}  {details[2]}          {details[3]}")
-                else:
-                    print("\nNo service information found.")
+    if question.lower() == 'y':
+        directory_creator = DirectoryCreator()
+        while True:
+            directory_name = input("\nWrite the name of the new directory: ")
+            if directory_creator.is_valid_directory_name(directory_name):
+                directory_location = input(
+                    "\nSpecify the location where you want to create the new directory. "
+                    "If you want to store the directory in your current directory, just type 'here'.\n"
+                    "Example: '/path/to/store/' or 'here': ")
+                directory_creator.create_directory(directory_location, directory_name)
+                break
             else:
-                print("\nNo service information found.")
-    except FileNotFoundError:
-        print(f"\nFile '{file_path}' not found.")
+                print("\nThe name you wrote is not valid for a directory. Please be careful!!")
 
-# Prompt user for target IP address
-target = input("\nProvide the target IP address: ")
-
-question = input("\nThis program creates files. If a file exists with the same name, it will overwrite it. "
-                     "To avoid future problems, the program will move or create a new directory.\n\n"
-                     "Do you want to proceed with the process of creating a directory? (y/n): ")
-
-if question.lower() == 'y':
-    while True:
-        directory_name = input("\nWrite the name of the new directory: ")
-        if is_valid_directory_name(directory_name):
-            directory_location = input(
-                "\nSpecify the location where you want to create the new directory. "
-                "If you want to store the directory in your current directory, just type 'here'.\n"
-                "Example: '/path/to/store/' or 'here': "
-            )
-            create_directory(directory_location, directory_name)
-            break
-        else:
-            print("\nThe name you wrote is not valid for a directory. Please be careful!!")
+    # Run nmap scan and save results to FullPorts.gnmap
+    print("\nPerfect, Now for the next scan we will perform a Stealth Scan so we are going to need sudo privileges\n")
     
-# Run nmap scan and save results to FullPorts.gnmap
-print("\nPerfect, Now for the next scan we will perform a Stealth Scan so we are going to need sudo privileges\n")
-subprocess.run(['sudo', 'nmap', '-sS', '-p-', '--open', '--min-rate', '2000', '-n', '-Pn', '-oG', 'FullPorts.gnmap', target], check=True)
+    print("\n#### RUNNING FULLPORTS SCAN#####\n")
+    
+    with tqdm(total=100, desc="Scanning", unit="%", bar_format="{desc}: {percentage:.0f}%|{bar}|") as pbar:
+        try:
+            subprocess.run(['sudo', 'nmap', '-sS', '-p-', '--open', '--min-rate', '2000', '-n', '-Pn', '-oG', 'FullPorts.gnmap', target], check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as e:
+            print(f"Error during the scan: {e}")
+        pbar.update(100)
+    print("\n#### SCAN END #####\n")
+    open_ports = PortScanner.extract_open_ports('FullPorts.gnmap')
 
-# Extract open ports from FullPorts.gnmap
-open_ports = extract_open_ports('FullPorts.gnmap')
+    # Copy open ports to clipboard
+    if open_ports:
+        ports_str = ', '.join(open_ports)
+        copy_to_clipboard(ports_str)
 
-# Copy open ports to clipboard
-if open_ports:
-    ports_str = ', '.join(open_ports)
-    copy_to_clipboard(ports_str)
+        # Run the second scan using the open ports
+        PortScanner.run_second_scan(open_ports, target)
 
-    # Run the second scan using the open ports
-    run_second_scan(open_ports, target)
+if __name__ == "__main__":
+    main()
